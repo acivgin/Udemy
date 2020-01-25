0b0925d53b644f7d97fc4d2f4ece78e61b0d2c17
Adis Civgin <civgin.adis@hotmail.com>
2020-01-25 22:50:36 +0100

    Initial Commit

---
+++ Udemy.Client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Udemy - ASP.NET CORE & Angular 8';
+}
