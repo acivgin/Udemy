bfb9386a4c9ddb76708be8e81859709157ed0bff
Adis Civgin <civgin.adis@hotmail.com>
2020-02-05 00:13:06 +0100

    Added decodedToken & persist username

--- Udemy.Client/src/app/_services/auth.service.ts
+++ Udemy.Client/src/app/_services/auth.service.ts
@@ -9,6 +9,7 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 export class AuthService {
   baseUrl = 'https://localhost:5001/api/auth/';
   jwtHelper = new JwtHelperService();
+  decodedToken: any;
 
   constructor(private http: HttpClient) {}
 
@@ -18,10 +19,12 @@ export class AuthService {
         const user = response;
         if (user) {
           localStorage.setItem('token', user.token);
+          this.decodedToken = this.jwtHelper.decodeToken(user.token);
         }
       })
     );
   }
+
   register(model: any) {
     return this.http.post(this.baseUrl + 'register', model);
   }
