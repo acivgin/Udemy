ded30921e869bacad4c149f15dee81ef8f705c31
Adis Civgin <civgin.adis@hotmail.com>
2020-02-04 23:33:49 +0100

    Added alertyfy JS

--- Udemy.Client/src/app/nav/nav.component.ts
+++ Udemy.Client/src/app/nav/nav.component.ts
@@ -1,5 +1,6 @@
 import { AuthService } from './../_services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-nav',
@@ -9,17 +10,20 @@ import { Component, OnInit } from '@angular/core';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private alertify: AlertifyService
+  ) {}
 
   ngOnInit() {}
 
   login() {
     this.authService.login(this.model).subscribe(
       next => {
-        console.log('Logged in sucessfully');
+        this.alertify.success('Logged in sucessfully');
       },
       error => {
-        console.log(error);
+           this.alertify.error(error);
       }
     );
   }
@@ -31,6 +35,6 @@ export class NavComponent implements OnInit {
 
   logout() {
     localStorage.removeItem('token');
-    console.log('Sucessfully logged out!');
+    this.alertify.message('Sucessfully logged out!');
   }
 }
--- Udemy.Client/src/app/register/register.component.ts
+++ Udemy.Client/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { AuthService } from './../_services/auth.service';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { AlertifyService } from '../_services/alertify.service';
 
 @Component({
   selector: 'app-register',
@@ -10,16 +11,19 @@ export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private alertify: AlertifyService) {}
 
   ngOnInit() {}
 
   register() {
-    this.auth.register(this.model).subscribe(() => {
-      console.log('Registration Succefully');
-    }, error => {
-        console.log(error);
-    });
+    this.auth.register(this.model).subscribe(
+      () => {
+        this.alertify.success('Registration Succefully');
+      },
+      error => {
+        this.alertify.error(error);
+      }
+    );
   }
 
   cancel() {
