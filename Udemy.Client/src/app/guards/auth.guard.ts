9907a21fae5a0d2314db2b96446d8c53d38f41d6
Adis Civgin <civgin.adis@hotmail.com>
2020-02-05 16:10:13 +0100

    Updated alertify message

--- Udemy.Client/src/app/guards/auth.guard.ts
+++ Udemy.Client/src/app/guards/auth.guard.ts
@@ -17,7 +17,7 @@ export class AuthGuard implements CanActivate {
     if (this.authService.loggedIn()) {
       return true;
     } else {
-      this.alertify.error('You shall not pass !');
+      this.alertify.error('You should be logged in!');
       this.router.navigate(['/home']);
       return false;
     }
